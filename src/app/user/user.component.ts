ef86a8f1282cf82b4727820a4a5dbebdd50e9f93
Mercy <mercycherotich757@gmail.com>
2022-02-11 15:31:42 +0300

    create a service for github

---
+++ src/app/user/user.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css']
+})
+export class UserComponent implements OnInit {
+
+users:string[] | undefined 
+
+getUsers(){
+  
+}
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
