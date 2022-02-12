4fa1c916f51807d786fa8cbf212922f574c84ded
Mercy <mercycherotich757@gmail.com>
2022-02-12 16:14:09 +0300

    create a get repo details in the service

--- src/app/github.service.ts
+++ src/app/github.service.ts
@@ -2,26 +2,29 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import {environment } from '../environments/environment';
 import {User} from './user-class/user';
+import {Repo} from './repo-class/repo';
 @Injectable({
   providedIn: 'root'
 })
 export class GithubService {
-  user!:  User
-  
-  getUsers(){
-    return this.user
-  }
+  user!: User;
+  repo!:Repo;
+
+ 
   constructor(private http:HttpClient) {
     this.user = new User("","");
+    this.repo = new Repo(0,"");
     console.log("service is ready");
   }
+
+
   profileRequest(){
     interface ApiResponse{
       name:string;
       bio:string;
     }
     let promise = new Promise<void>((resolve,reject)=>{
-      this.http.get<ApiResponse>(environment.api_key).toPromise().then(response=>{
+      this.http.get<ApiResponse>(environment.apiUrl).toPromise().then(response=>{
         this.user.name = response!.name
         this.user.bio = response!.bio
 
@@ -36,7 +39,25 @@ export class GithubService {
     })
     return promise
   }
-  // getProfileInfo(){
-  //   return this.http.get("https://api.github.com/users/" + this.username +"?api_key=" + this.api_key);
-  // }
+  repoRequest(){
+    interface ApiResponse{
+      name:string;
+      bio:string;
+    }
+    let promise = new Promise<void>((resolve,reject)=>{
+      this.http.get<ApiResponse>(environment.apiUrl).toPromise().then(response=>{
+        this.user.name = response!.name
+        this.user.bio = response!.bio
+
+        resolve()
+      },
+      error=>{
+        this.user.name = "Never, never, never give up"
+        this.user.bio = "Winston Churchill"
+
+        reject(error)
+      })
+    })
+    return promise
+  }
 }
